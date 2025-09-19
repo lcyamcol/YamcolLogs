5029e286ff32ff88116457a516171931262fcf98
lcyamcol <luan.castilho@yamcol.com.br>
2025-09-19 16:50:47 -0300

    chore: adiciona path da endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,28 @@
 import { Elysia, t } from "elysia";
 import { YamcolLogService } from "./core/yamcol-log.service";
 import { LogRepositoryPg } from "./infra/log.repository";
+import { checkBanco, poolInfo } from "./infra/database";
+import { formatarBytes } from "./libs";
 const PORT: number = +(process.env.PORT || 8081);
 const NODE_ENV = process.env.NODE_ENV ?? "development";
 
 const app = new Elysia();
 const yamcolLogService = new YamcolLogService(new LogRepositoryPg());
 
-app.get("/", () => "Rodando API com Elysia e Bun!");
+app.get("/saude", async () => {
+  const bancoVerificado = await checkBanco();
+  const memoria = process.memoryUsage();
+
+  return {
+    mensagem: "Serviço de logs vivo", sucesso: true, data: new Date(), ambiente: NODE_ENV, versaoRuntime: process.version, uptime: process.uptime(), memoria: {
+      rss: formatarBytes(memoria.rss),
+      heapTotal: formatarBytes(memoria.heapTotal),
+      heapUsed: formatarBytes(memoria.heapUsed),
+      external: formatarBytes(memoria.external),
+      arrayBuffers: formatarBytes(memoria.arrayBuffers),
+    }, bancoVivo: bancoVerificado, pools: poolInfo
+  };
+});
 
 app.post("/logs", ({ body }) => {
   return yamcolLogService.save(body);
